a2da0657fd327bbeff1cdd4114e8aa528f9797c4
Anton Bowden <bowden.anton@gmail.com>
2017-08-14 02:41:16 +0100

    refactor table into separate Chart components

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
-class WeatherList extends Component {
+class WeatherList extends React.Component {
+
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+
+    console.log(name, temps);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,7 +35,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
