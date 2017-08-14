562dd689ee84c875a5dade5f126f1b467d4c7cc7
Anton Bowden <bowden.anton@gmail.com>
2017-08-14 01:00:24 +0100

    fetch weather data from API

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,12 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export default fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},uk`;
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},gb`;
   const request = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
     payload: request
   };
-}
+};
